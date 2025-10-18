a5600f9440c27c759e567d04a106b5f344e6b7b3
etzjunior <victorwoha233@gmail.com>
2025-10-18 22:53:54 +0200

    Show Statistics feature

--- projects/grade_tracker/grade_tracker.py
+++ projects/grade_tracker/grade_tracker.py
@@ -8,8 +8,19 @@ def add_student():
     students[names] = grades # adds student name and grades to the dictionary
     print(f"{names} added successfully!\n")
 
-add_student()
-add_student()
+
+def view_students():
+    if not students:
+        print("No students available.\n")
+    else:
+        print("--- Student List ---")
+        for name, grade in students.items():
+            print(f"{name}: {grade}")
+
 
 print("Current students dictionary:")
 print(students)
+
+add_student()
+add_student()
+view_students()
