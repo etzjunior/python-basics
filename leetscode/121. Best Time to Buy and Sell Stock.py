f1e3633cab963d59f7bf37db9b4816548fb79137
etzjunior <victorwoha233@gmail.com>
2025-10-19 14:30:42 +0200

    121. Best Time to Buy and Sell Stock

---
+++ leetscode/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        max_profit = 0
+        for i in range(len(prices)):
+            for j in range(i+1, len(prices)):
+               profit = prices[j] - prices[i]
+               max_profit = max(max_profit, profit)
+        return max_profit
